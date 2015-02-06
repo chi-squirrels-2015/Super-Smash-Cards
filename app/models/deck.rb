9c56b39d00be718a34a9d0cbe1d67b9d458908d1
Brian Sheridan & John N. Mandalakas & KYoon105 <pair+bsheridan12.jnmandal.kyoon105@devbootcamp.com>
2015-02-06 16:56:07 -0600

    populate models, tables, and user seed data

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,3 @@
 class Deck < ActiveRecord::Base
-  # Remember to create a migration!
+  has_many :cards
 end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.create(username: "Kyoon", password: "1234")
+User.create(username: "Jmandal", password: "abcd")
+User.create(username: "Bsheridan", password: "wxyz")
